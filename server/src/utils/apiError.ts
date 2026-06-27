9848a33db3c88438ed199000f035bfae8fddc074
Vishal Dhiman <vishaldhimann27@gmail.com>
2026-06-27 13:11:19 +0530

    feat: add async handler and custom api error

---
+++ server/src/utils/apiError.ts
@@ -0,0 +1,15 @@
+class ApiError extends Error {
+    public statusCode: number;
+
+    constructor(
+        statusCode: number,
+        message: string
+    ) {
+        super(message);
+
+        this.statusCode = statusCode;
+        this.name = "ApiError";
+        
+        Error.captureStackTrace(this, this.constructor)
+    }
+}
---
+++ server/src/utils/asyncHandler.ts
@@ -0,0 +1,11 @@
+import { Response, Request, NextFunction } from "express";
+
+const asyncHandler = (fn: (req: Request, res: Response, next: NextFunction) => Promise<any>) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        Promise.resolve(fn(req, res, next)).catch((error) => {
+            next(error)
+        })
+    }
+};
+
+export default asyncHandler;
