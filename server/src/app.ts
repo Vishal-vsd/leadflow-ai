aacda666bb7539088ad0296e30f16141af09d05f
Vishal Dhiman <vishaldhimann27@gmail.com>
2026-06-28 16:37:43 +0530

    feat: implement user registration endpoint

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,7 @@
 import cookieParser from "cookie-parser";
 import cors from "cors"
 import express from "express";
-
+import authRoutes from "./routes/authRoutes"
 const app = express();
 
 app.use(cors({
@@ -10,6 +10,7 @@ app.use(cors({
 }))
 app.use(express.json());
 app.use(cookieParser())
+app.use("/api/auth", authRoutes)
 
 app.get("/api/health", (req, res) => {
 
---
+++ server/src/controllers/authcontrollers.ts
@@ -0,0 +1,54 @@
+import { Request, Response } from "express";
+import ApiError from "../utils/apiError";
+import User from "../models/User";
+import ApiResponse from "../utils/apiResponse";
+import asyncHandler from "../utils/asyncHandler";
+
+export const registerUser = asyncHandler(
+  async (req: Request, res: Response) => {
+
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      throw new ApiError(
+        400,
+        "All fields are required"
+      );
+    }
+
+    const existingUser =
+      await User.findOne({ email });
+
+    if (existingUser) {
+      throw new ApiError(
+        409,
+        "User already exists"
+      );
+    }
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+
+    const createdUser =
+      await User.findById(user._id)
+        .select("-password");
+
+    if (!createdUser) {
+      throw new ApiError(
+        500,
+        "Failed to create user"
+      );
+    }
+
+    return res.status(201).json(
+      new ApiResponse(
+        201,
+        createdUser,
+        "User registered successfully"
+      )
+    );
+  }
+);
