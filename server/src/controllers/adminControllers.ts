34a3eb8ed3a4916c3d598e2cfd48f1e31d3ccc2f
Vishal Dhiman <vishaldhimann27@gmail.com>
2026-07-21 16:05:52 +0530

    feat: implement admin user management with search and pagination

--- server/src/controllers/adminControllers.ts
+++ server/src/controllers/adminControllers.ts
@@ -253,3 +253,93 @@ export const getAllConversionStats = asyncHandler(
         )
     }
 )
+
+export const getAllUsers = asyncHandler(
+    async(req: Request, res: Response) => {
+       const page = Number(req.query.page) || 1;
+       const limit = Number(req.query.limit) || 10;
+       const search = req.query.search as string;
+       const sort =  req.query.sort as string;
+
+       const skip = (page - 1) * limit;
+
+
+       const matchQuery: any = {};
+
+       let sortOption: any = {
+        createdAt: -1
+       }
+
+       if(search) {
+        matchQuery.$or = [
+            {
+                name: {
+                    $regex: search,
+                    $options: "i"
+                }
+            },
+            {
+                email: {
+                    $regex: search,
+                    $options: "i",
+                }
+            },
+        ]
+       }
+
+       if(sort === "oldest"){
+        sortOption = {createdAt: 1}
+       }
+
+       const result = await User.aggregate([
+        {
+            $match: matchQuery
+        },
+        {
+            $facet: {
+                users: [
+                    {
+                        $sort: sortOption
+                    },
+                    {
+                        $skip: skip
+                    },
+                    {
+                        $limit: limit
+                    },
+                    {
+                        $project: {
+                            password: 0,
+                            refreshToken: 0
+                        }
+                    }
+                ],
+                totalCount: [
+                    {
+                        $count: "count"
+                    }
+                ]
+            }
+        }
+       ])
+
+       const users = result[0].users;
+
+       const totalUsers = result[0].totalCount[0]?.count || 0;
+
+       const totalPages = Math.ceil(totalUsers/limit);
+
+       return res.status(200).json(
+        new ApiResponse(
+            200,
+            {
+                users,
+                totalUsers,
+                currentPage: page,
+                totalPages
+            },
+            "Users fetched successfully"
+        )
+       )
+    }
+)
