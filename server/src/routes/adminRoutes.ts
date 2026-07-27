53cfcd0d2c7a89f6107eccdd9471a143e2cc7411
Vishal Dhiman <vishaldhimann27@gmail.com>
2026-07-27 14:39:43 +0530

    feat: add admin dashboard endpoint

--- server/src/routes/adminRoutes.ts
+++ server/src/routes/adminRoutes.ts
@@ -10,48 +10,47 @@ import {
     getAllConversionStats,
     getAllUsers,
     updateUserRole,
+    getDashboardData,
 } from "../controllers/adminControllers";
 
 const router = express.Router();
 
+router.use(authMiddleware);
+router.use(authorize("admin"))
+
 // Lead Management
 router.get(
     "/leads",
-    authMiddleware,
-    authorize("admin"),
     getAllLeads
 );
 
 // Analytics
 router.get(
     "/lead-stats",
-    authMiddleware,
-    authorize("admin"),
     getAllLeadStats
 );
 
 router.get(
     "/source-stats",
-    authMiddleware,
-    authorize("admin"),
     getAllSourceStats
 );
 
 router.get(
     "/conversion-stats",
-    authMiddleware,
-    authorize("admin"),
     getAllConversionStats
 );
 
 // User Management
 router.get(
     "/users",
-    authMiddleware,
-    authorize("admin"),
     getAllUsers
 );
 
-router.patch("/users/:id/role", authMiddleware, authorize("admin"), updateUserRole)
+router.patch("/users/:id/role", updateUserRole)
+
+router.get(
+    "/dashboard",
+    getDashboardData
+);
 
 export default router;
