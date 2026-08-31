4ea97fd9e7bd0abdacd77db24986197f85cd442a
Vishal Dhiman <vishaldhimann27@gmail.com>
2026-09-01 04:22:46 +0530

    feat: add auth context and role support

---
+++ client/src/routes/AdminRoute.tsx
@@ -0,0 +1,17 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "@/context/AuthContext";
+
+const AdminRoute = () => {
+    const { user, isAuthenticated } = useAuth();
+
+    if(isAuthenticated){
+        return <Navigate to="/login" replace/>
+    }
+    if(user?.role !== "admin"){
+        return <Navigate to="/" replace />
+    }
+
+    return <Outlet />
+}
+
+export default AdminRoute;
